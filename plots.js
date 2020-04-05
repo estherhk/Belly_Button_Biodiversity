ff616ea6aae75a475282d1006893c4e856a4feb4
estherhk <37747392+estherhk@users.noreply.github.com>
2020-04-05 09:32:59 -0700

    Add files via upload

--- plots.js
+++ plots.js
@@ -1,181 +1,3 @@
-// Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
-
-// var trace = {
-//     x: ["burrito", "pizza", "chicken"],
-//     y: [10, 18, 5],
-//     type: "bar"
-// };
-
-// var layout = {
-//     title: "Luncheon Survey",
-//     xaxis: {title: "Food Option"},
-//     yaxis: {title: "Number of Respondents"}
-// };
-
-// Plotly.newPlot("plotArea", [trace], layout);
-
-
-// // var beverages = {
-// //     x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini",
-// //     "nonalcoholic margarita", "ice tea", "nonalcoholic rum&cake", "nonalcoholic maitai", "nonalcoholic gin&tonic"],
-// //     y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-// //     type: "bar"
-// // };
-
-// // var layout = {
-// //  title: "'Bar' Chart",
-// //  xaxis: { title: "Drinks"},
-// //  yaxis: { title: "% of Drinks Ordered"}
-// // };
-
-// // Plotly.newPlot("drinks", [beverages], layout);
-
-// // Creating a Bar Chart
-// var trace = {
-//  x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-//  y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//  type: "bar"
-// };
-// var data = [trace];
-// var layout = {
-//  title: "'Bar' Chart",
-//  xaxis: { title: "Drinks"},
-//  yaxis: { title: "% of Drinks Ordered"}
-// };
-// Plotly.newPlot("plotArea", data, layout);
-
-// // Creating a Pie Chart
-// var trace = {
-//     labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-//     "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-//     values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//     type: 'pie'
-//    };
-//    var data = [trace];
-//    var layout = {
-//     title: "'Bar' Chart",
-//    };
-// Plotly.newPlot("plotArea", data, layout);
-
-// // Creating a Scatter Chart
-// var trace = {
-//     x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-//     "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-//     y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//     mode: 'markers',
-//     type: 'scatter'
-//    };
-//    var data = [trace];
-//    var layout = {
-//     title: "'Bar' Chart",
-//    };
-// Plotly.newPlot("plotArea", data, layout);
-
-// //SKILL DRILL Practice map() method
-// var numbers = [0,2,4,6,8];
-// var add = numbers.map(function(plus){
-//     return plus + 5;
-// });
-// console.log(add);
-
-// //SKILL DRILL Practice map () method with specific object
-// cities = [
-//     {
-//       "Rank": 1,
-//       "City": "San Antonio ",
-//       "State": "Texas",
-//       "Increase_from_2016": "24208",
-//       "population": "1511946"
-//     },
-//     {
-//       "Rank": 2,
-//       "City": "Phoenix ",
-//       "State": "Arizona",
-//       "Increase_from_2016": "24036",
-//       "population": "1626078"
-//     },
-//     {
-//       "Rank": 3,
-//       "City": "Dallas",
-//       "State": "Texas",
-//       "Increase_from_2016": "18935",
-//       "population": "1341075"
-//     }
-// ];
-
-// cityPopulation = cities.map(function(city){
-//     return city.population;
-// });
-// console.log(cityPopulation);
-
-// //SKILL DRILL Practice filter() method
-// // var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
-// // var animals = words.filter(function(letter){
-// //     return letter [0] == "s";
-// // });
-// var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
-// var animals = words.filter(letter => letter.startsWith("s"));
-// console.log(animals);
-
-// // SKILL DRILL slice() method
-// var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
-// slice1 = words.slice(3, );
-
-// // Sort and Select Cities
-// var sortedCities = cityGrowths.sort((a,b) => a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
-// var topFiveCities = sortedCities.slice(0,7);
-
-// // Create Arrays of City Names & Growth Figures
-// var topFiveCityNames = topFiveCities.map(city => city.City);
-// var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
-
-// // Create a Bar Chart with Arrays
-// var trace = {
-//     x: topFiveCityNames,
-//     y: topFiveCityGrowths,
-//     type: "bar"
-//   };
-//   var data = [trace];
-//   var layout = {
-//     title: "Most Rapidly Growing Cities",
-//     xaxis: { title: "City" },
-//     yaxis: { title: "Population Growth, 2016-2017"}
-//   };
-//   Plotly.newPlot("bar-plot", data, layout);
-
-  //Dropdown
-  // function init() {
-  //   data = [{
-  //     x: [1, 2, 3, 4, 5],
-  //     y: [1, 2, 4, 8, 16] }];
-  //   Plotly.newPlot("plot", data);
-  // };
-  
-  // d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-  // function updatePlotly() {
-  //   var dropdownMenu = d3.select("#dropdownMenu");
-  //   var dataset = dropdownMenu.property("value");
-  
-  //   var xData = [1, 2, 3, 4, 5];
-  //   var yData = [];
-  
-  //   if (dataset === 'dataset1') {
-  //     yData = [1, 2, 4, 8, 16];
-  //   };
-  
-  //   if (dataset === 'dataset2') {
-  //     yData = [1, 10, 100, 1000, 10000];
-  //   };
-  
-  //   var trace = {
-  //     x: [xData],
-  //     y: [yData],
-  //   };
-  //   Plotly.restyle("plot", trace);
-  // };
-  
-  // init();
-
 function init() {
   var selector = d3.select("#selDataset");
   
@@ -188,7 +10,10 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-})}
+    const firstSample = sampleNames[0];
+    buildMetadata(firstSample);
+    buildCharts(firstSample);
+  })}
   
 init();
 
@@ -206,23 +31,109 @@ function buildMetadata(sample) {
       
     PANEL.html("");
     PANEL.append("h6").text(`ID: ${result.id}`);
-    PANEL.append("h6").text(result.ethnicity);
-    PANEL.append("h6").text(result.gender);
-    PANEL.append("h6").text(result.age);
-    PANEL.append("h6").text(result.location);
-    PANEL.append("h6").text(result.bbtype);
-    PANEL.append("h6").text(result.wfreq);
+    PANEL.append("h6").text(`Ethnicity: ${result.ethnicity}`);
+    PANEL.append("h6").text(`Gender: ${result.gender}`);
+    PANEL.append("h6").text(`Age: ${result.age}`);
+    PANEL.append("h6").text(`Location: ${result.location}`);
+    PANEL.append("h6").text(`Bbtype: ${result.bbtype}`);
+    PANEL.append("h6").text(`Wfreq: ${result.wfreq}`);
   });
 };
 
-function buildCharts(sample) {
+//Create Bar Chart
+function barChart(sample){
   d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    var PANEL = d3.select("#sample-metadata");
+    var samples= data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var currentSample = resultArray[0];
 
-    PANEL.html("");
-    PANEL.append("h6").text(result.location);
+    var barSamples = {
+      x: currentSample.sample_values.slice(0,10).reverse(),
+      y: currentSample.otu_ids.slice(0,10).map(otuID=> "OTU " + otuID.toString()).reverse(),
+      text: currentSample.otu_labels.reverse(),
+      type: 'bar',
+      orientation: 'h'
+    };
+    var layout = {
+      title: "<b>Belly Button Top 10 OTUs</b>",
+      xaxis: { title: "Sample Values"},
+      yaxis: { title: "OTU IDs"}
+    };
+    
+    Plotly.newPlot("bar", [barSamples], layout);
   });
+}
+
+//Create Gauge Chart
+function gaugeChart(sample){
+  d3.json("samples.json").then((data) => {
+    var samples= data.metadata;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var currentSample = resultArray[0];
+  
+    var gaugeSamples = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: currentSample.wfreq,
+        title: "<b>Belly Button Washing Frequency</b> <br>Scrubs per Week",
+        type: 'indicator',
+        mode: 'gauge+number',
+        gauge: {
+          bar: {color: "1ea4b0"},
+          axis: {range: [0,9]},
+          steps: [
+            {range: [0,2], color: "#f7f2eb"},
+            {range: [2,4], color:"#e8e6c8"},
+            {range: [4,6], color: "#d4e494"},
+            {range: [6,8], color: "#86be7e"},
+            {range: [8,9], color: "#7fb485"}  
+          ]
+          }
+      }
+    ]; 
+    var layout = { 
+      width: 600, height: 500, margin: { t: 0, b: 0 } 
+    };
+
+    Plotly.newPlot('gauge', gaugeSamples, layout);
+  });
+}
+
+//Create Bubble Chart
+function bubbleChart(sample){
+  d3.json("samples.json").then((data) => {
+    var samples= data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var currentSample = resultArray[0];
+
+    var bubbleSamples = {
+      x: currentSample.otu_ids,
+      y: currentSample.sample_values,
+      text: currentSample.otu_labels,
+      mode: 'markers',
+      marker: {
+        color: currentSample.otu_ids,
+        colorscale: 'Earth',
+        opacity: [1, 0.8, 0.6, 0.4],
+        size: currentSample.sample_values.map(sampleSize => sampleSize/2) 
+      }
+    };
+
+    var layout = {
+      title: "<b>Belly Button Bacterias</b>",
+      showlegend: false,
+      xaxis: { title: "OTU ID"},
+      yaxis: { title: "Sample Values"}
+    };
+    
+    Plotly.newPlot("bubble", [bubbleSamples], layout);
+  });
+}  
+
+
+// Create Charts
+function buildCharts(sample) {
+ barChart(sample)
+ gaugeChart(sample)
+ bubbleChart(sample)
 }
